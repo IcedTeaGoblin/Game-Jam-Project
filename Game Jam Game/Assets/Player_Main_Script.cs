b103d1e32bb14058edb5292834bb8b5327d3ec94
Shmubler <tacogoboom@gmail.com>
2019-09-28 12:47:33 -0700

    no message

---
+++ Game Jam Game/Assets/Player_Main_Script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Main_Script : MonoBehaviour
+{
+    private float y_movement;
+    private float x_movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ///Check for movement
+        transform.position += new Vector3(x_movement, y_movement, 0.000f);
+
+        ///Movement input
+        if (Input.GetKey(KeyCode.A))
+        {
+            x_movement = -0.100f;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            x_movement = 0.100f;
+        }
+        else
+        {
+            x_movement = 0.000f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            y_movement = 0.050f;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            y_movement = -0.050f;
+        }
+        else
+        {
+            y_movement = 0.000f;
+        }
+    }
+
+}
---
+++ Game Jam Game/Assets/Timer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    public Rigidbody2D Collectable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = mainTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+        }
+        
+        else if(timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            timer = 0.0f;
+
+            ///Create an Object
+            Instantiate(Collectable, new Vector3(Random.Range(-14.0f, 14.0f), 6.0f, 0.0f), Quaternion.identity);
+
+            timerReset();
+        }
+    }
+
+    public void timerReset()
+    {
+        canCount = true;
+        doOnce = false;
+        timer = Random.Range(1.00f, 4.00f);
+    }
+}
