3eea555295968c79d8c050bf9f7121a30ed1b29a
Ahyouss <adrianyoussef@gmail.com>
2019-09-28 18:00:03 -0700

    menu scripts

---
+++ Game Jam Game/Assets/Scripts/Scene_Switcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Switcher : MonoBehaviour
+{
+    public void GotoMainGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void GotoHowToPlayScene()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
